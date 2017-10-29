b7181856a1882fd6057c5117dccb16afa82df4b2
Guilherme Welter <guilherme.a.welter@gmail.com>
2017-10-29 20:42:25 -0200

    Adicionando adapter à listview e criando layout novo para listview de categorias

--- app/src/main/java/com/example/guilherme/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/guilherme/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.guilherme.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.guilherme.coderswag.Model.Category
 import com.example.guilherme.coderswag.R
+import com.example.guilherme.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        category_listview.adapter = adapter
     }
 }
--- app/src/main/java/com/example/guilherme/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/guilherme/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.guilherme.coderswag.Model
 /**
  * Created by guilherme on 29/10/17.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
